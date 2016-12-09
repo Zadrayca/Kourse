e305c168cbc78ad42fa957284a641316d87e3c28
Zadrayca <coxatiy@gmail.com>
2016-12-09 20:43:06 +0300

    23

---
+++ paket/calc.py
@@ -0,0 +1,14 @@
+print('имя модуля: {}'.format(__name__))
+
+def summa(a, b):
+    return a + b
+
+def multi(a, b):
+    return a * b
+
+def pow(a, n):
+    return a ** n
+
+
+if __name__ == '__main__':
+    print('qweqweqwedfsdfsdfmkmfkvmfv')
---
+++ paket/main.py
@@ -0,0 +1,9 @@
+from calc import summa, multi, pow
+
+print('имя модуля: {}'.format(__name__))
+
+
+y = multi(3, 5)
+x = summa(5, 7)
+z = pow(3, 5)
+print(x, y, z)
