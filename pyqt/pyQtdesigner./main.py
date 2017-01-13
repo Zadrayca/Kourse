58713fb3d363b0ff11d149ea25938e6c8e90950d
Zadrayca <coxatiy@gmail.com>
2017-01-13 22:08:51 +0300

    13.01.2017 21.50

---
+++ pyqt/pyQtdesigner./main.py
@@ -0,0 +1,26 @@
+
+import sys
+from PyQt5.QtWebKitWidgets import QWebView
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.uic import loadUi
+
+
+class Browzer(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.initUi()
+
+    def initUi(self):
+        loadUi('mainwindow.ui', self)
+        self.statusBar().showMessage('Приложение запущенно')
+        self
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    browzer = Browzer()
+    browzer.show()
+
+    sys.exit(app.exec_())
