4d0efa851b572b83489d3e00c2fcf914ad68fd36
Zadrayca <coxatiy@gmail.com>
2017-01-16 22:04:38 +0300

    16.01.2017 22.04

--- pyqt/pyQtdesigner./main.py
+++ pyqt/pyQtdesigner./main.py
@@ -1,18 +1,71 @@
 
 import sys
+import stuff_rc
 
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 from PyQt5.uic import loadUi
-import PyQt5.QtWebEngineWidgets
+
+try:
+    from PyQt5.QtWebEngineWidgets import QWebEngineViev as QWebView
+except ImportError:
+    from PyQt5.QtWebKitWidgets import QWebView
+
 
 class Browzer(QMainWindow):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.initUi()
+        self.init_signals()
 
     def initUi(self):
         loadUi('mainwindow.ui', self)
-        self.statusBar().showMessage('Приложение запущенно')
+
+        self.webView = QWebView(self)
+        self.mainLayout.addWidget(self.webView)
+        self.init_history_buttons()
+
+
+
+    def init_signals(self):
+        self.actionAboutQt.triggered.connect(self.about_qt)
+
+        self.backBtn.clicked.connect(self.webView.back)
+        self.forwardBtn.clicked.connect(self.webView.forward)
+        self.query.returnPressed.connect(self.on_enter_query)
+        self.searchBtn.clicked.connect(self.on_enter_query)
+
+        self.webView.urlChanged.connect(self.init_history_buttons)
+        self.webView.urlChanged.connect(self.set_query)
+        self.webView.titleChanged.connect(self.on_title_changed)
+        self.webView.page().linkHovered.connect(self.on_link_hovered)
+
+    def init_history_buttons(self):
+        history = self.webView.page().history()
+
+        self.backBtn.setEnabled(history.canGoBack())
+        self.forwardBtn.setEnabled(history.canGoForward())
+
+    def about_qt(self):
+        QMessageBox.aboutQt(self, 'O Qt')
+
+
+    def on_enter_query(self):
+        url = QUrl.fromUserInput(self.query.text())
+        self.webView.load(url)
+
+
+    def on_title_changed(self, title):
+        name = self.windowTitle().split(' | ').pop()
+        self.setWindowTitle(title + ' | ' + name)
+
+    def set_query(self, url):
+        if isinstance(url, QUrl):
+            self.query.setText(url.toString())
+
+    def on_link_hovered(self, url, *args, **kwargs):
+        self.statusBar().showMessage(url) # по идее url.toString()
+
 
 
 
